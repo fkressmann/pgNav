f7c9b7b7563341b0028cceb86a0cf7968d137e00
Stapper, Gerrit <gerrit.stapper@accenture.com>
2020-02-08 20:18:51 +0100

    Added routes and tooltips to preview the route info

--- pgnav-ui/src/app/angular-material.module.ts
+++ pgnav-ui/src/app/angular-material.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 const modules = [
   MatTableModule,
   MatCardModule,
-  MatSlideToggleModule
+  MatSlideToggleModule,
+  MatTooltipModule
 ];
 
 @NgModule({
--- pgnav-ui/src/app/app.module.ts
+++ pgnav-ui/src/app/app.module.ts
@@ -7,18 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TableViewListComponent } from './table-view-list/table-view-list.component';
 import { TableViewCardsComponent } from './table-view-cards/table-view-cards.component';
 import { TableViewComponent } from './table-view/table-view.component';
+import { TableDetailsComponent } from './table-details/table-details.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableViewListComponent,
     TableViewCardsComponent,
-    TableViewComponent
+    TableViewComponent,
+    TableDetailsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AngularMaterialModule // collects all required angular material modules
+    AngularMaterialModule, // collects all required angular material modules
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
