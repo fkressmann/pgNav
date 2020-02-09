8306386b7305c494e8f6a73ec6f04969fc11e4b8
Felix Kressmann <felix@felixkr.de>
2020-02-09 05:43:56 +0100

    Added Ref-To and filtering for tables. Fixed error on empty tables

--- models/column.py
+++ models/column.py
@@ -3,13 +3,6 @@ from models.base import BaseModel
 
 class Column(BaseModel):
 
-    def __init__(self, name, table, dtype):
-        self.name = name
-        self.table = table
-        self.dtype = dtype
-        self.ref_to = []
-        self.ref_from = []
-
     def __init__(self, pg_column):
         self.name = pg_column.name
         self.ref_to = []
@@ -17,3 +10,6 @@ class Column(BaseModel):
 
     def add_ref_from(self, ref):
         self.ref_from.append(ref)
+
+    def add_ref_to(self, ref):
+        self.ref_to.append(ref)
