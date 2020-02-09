3b3962632652a9308690880b70037e919c5fef73
felix <emilfelix@live.de>
2020-02-09 05:43:56 +0100

    Added Ref-To and filtering for tables. Fixed error on empty tables

--- services/database_service.py
+++ services/database_service.py
@@ -8,7 +8,7 @@ parser = re.compile("FOREIGN KEY \\(([\\w_]*)\\) REFERENCES ([\\w_]*)\\(([\\w_]*
 
 from models.table import Table
 from models.column import Column
-from models.ref_from import RefFrom
+from models.refs import Reference
 
 
 class DatabaseService:
@@ -41,21 +41,31 @@ class DatabaseService:
         tables = [Table(name[0]) for name in result]
         return tables
 
-    def select(self, table_name, limit=25):
+    def select(self, table_name, limit=25, filter_column=None, filter_value=None):
         oid = self.get_table_oid(table_name)
-
-        query = sql.SQL("SELECT * FROM {} LIMIT %s").format(sql.Identifier(table_name))
-        self.dict_cursor.execute(query, (limit,))
+        if filter_column and filter_value:
+            query = sql.SQL("SELECT * FROM {} WHERE {}={} LIMIT %s").format(
+                sql.Identifier(table_name),
+                sql.Identifier(filter_column),
+                sql.Placeholder())
+            self.dict_cursor.execute(query, (filter_value, limit))
+        else:
+            query = sql.SQL("SELECT * FROM {} LIMIT %s").format(sql.Identifier(table_name))
+            self.dict_cursor.execute(query, (limit,))
+        print(self.dict_cursor.query)
         ddl = self.dict_cursor.description
         columns = [Column(c) for c in ddl]
         data = self.dict_cursor.fetchall()
 
-        refs = self.get_ref_from(oid)
-        print(refs)
-
-        for ref in refs:
+        refs_from = self.get_ref_from(oid)
+        for ref in refs_from:
             f = filter(lambda column: column.name == ref.source_col, columns)
             next(f).add_ref_from(ref)
+
+        refs_to = self.get_ref_to(oid)
+        for ref in refs_to:
+            f = filter(lambda column: column.name == ref.source_col, columns)
+            next(f).add_ref_to(ref)
 
         return Table(table_name, oid, columns, data)
 
@@ -72,10 +82,19 @@ class DatabaseService:
         self.cursor.execute(query, (oid,))
         print(self.cursor.query)
         refs = self.cursor.fetchall()
-        print(refs)
         d_refs = []
         for ref in refs:
             regex = parser.search(ref[1])
             foreign_col = regex.group(1)
-            d_refs.append(RefFrom(regex.group(3), ref[0], foreign_col))
+            d_refs.append(Reference(regex.group(3), ref[0], foreign_col))
+        return d_refs
+
+    def get_ref_to(self, oid):
+        query = "SELECT pg_catalog.pg_get_constraintdef(r.oid, true) as condef FROM pg_catalog.pg_constraint r WHERE r.conrelid = %s AND r.contype = 'f' ORDER BY 1;"
+        self.cursor.execute(query, (oid,))
+        refs = self.cursor.fetchall()
+        d_refs = []
+        for ref in refs:
+            regex = parser.search(ref[0])
+            d_refs.append(Reference(regex.group(1), regex.group(2), regex.group(3)))
         return d_refs
