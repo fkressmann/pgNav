917fa35a8d90fd962774f4f9e14fdb4ac9de3362
Felix Kressmann <felix@felixkr.de>
2020-02-21 11:38:21 +0100

    * Moved caching of table structure infront of every new table request to not slow down startup * Returning BadRequest for wrong FE filtered selects to protect DB from failing queries

--- pgnav-be/models/table.py
+++ pgnav-be/models/table.py
@@ -8,13 +8,16 @@ class Table(BaseModel):
         self.name = name
         self.columns = []
         self.rows = []
-        self.all_rows_loaded = False
+        self.fetched = False
 
     def set_columns(self, columns):
         self.columns = columns
 
     def set_rows(self, rows):
         self.rows = rows
+
+    def get_column_names(self):
+        return list(map(lambda col: col.get_name(), self.columns))
 
     def __str__(self):
         return "Table: " + self.name
