74b91266f9f944022755c562bcc32b19a515b31c
Felix Kressmann <felix.kressmann@boehringer-ingelheim.com>
2020-03-14 20:47:02 +0100

    System endpoint to get health or shutdown the server

--- pgnav-be/__main__.py
+++ pgnav-be/__main__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 import webbrowser
-from threading import Timer
+# from threading import Timer
 # from werkzeug.middleware.profiler import ProfilerMiddleware
 
 from controllers.database_controller import DatabaseTableController, DatabaseConnectController, DatabaseTablesController
+from controllers.system_controller import SystemController, SystemCommandController
 
 app = Flask(__name__, static_url_path='')
 CORS(app)
@@ -16,6 +17,9 @@ prefix = "/api"
 api.add_resource(DatabaseTableController, prefix + '/table/<string:table>')
 api.add_resource(DatabaseConnectController, prefix + '/connect')
 api.add_resource(DatabaseTablesController, prefix + '/tables')
+api.add_resource(SystemController, prefix + '/system')
+api.add_resource(SystemCommandController, prefix + '/system/<string:action>')
+
 
 def open_browser():
     webbrowser.open_new('http://127.0.0.1:5000/')
@@ -27,5 +31,5 @@ def hello_world():
 
 
 if __name__ == "__main__":
-#    Timer(1, open_browser).start()
+    #    Timer(1, open_browser).start()
     app.run()
